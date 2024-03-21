76ea61a8a7efedd9aa76ad3f22d9de562c897f82
BenjaminMehrez <benjaminjofre01@gmail.com>
2024-03-20 21:29:42 -0300

    ajustando

--- react-js/src/components/NavBar/Menu.jsx
+++ react-js/src/components/NavBar/Menu.jsx
@@ -1,12 +1,10 @@
-import { useCartContext } from "../../Context/CartContext"
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
 
 import './Menu.css';
 
 function Menu() {
 
-    const { cantidadTotal } = useCartContext()
 
     const [showMenu, setShowMenu] = useState(false);
 
@@ -21,11 +19,11 @@ function Menu() {
             </button>
             <nav className={showMenu ? 'menu active' : 'menu'}>
                 <ul>
-                    <li><a href='/'>Inicio</a></li>
-                    <li><a href='/categoria/remeras'>Remeras</a></li>
-                    <li><a href='/categoria/zapatillas'>Zapatillas</a></li>
-                    <li><a href='/categoria/camperas'>Camperas</a></li>
-                    <li><a href='/categoria/conjuntos'>Conjuntos</a></li>
+                    <li><NavLink to='/'>Inicio</NavLink></li>
+                    <li><NavLink to='/categoria/remeras'>Remeras</NavLink></li>
+                    <li><NavLink to='/categoria/zapatillas'>Zapatillas</NavLink></li>
+                    <li><NavLink to='/categoria/camperas'>Camperas</NavLink></li>
+                    <li><NavLink to='/categoria/conjuntos'>Conjuntos</NavLink></li>
                 </ul>
             </nav>
         </div>
@@ -35,22 +33,3 @@ function Menu() {
 }
 
 export default Menu;
-
-
-
-
-{/* <div>
-<button className="menu-toggle" onClick={toggleMenu}>
-    {showMenu ? 'Cerrar' : 'Menú'}
-</button>
-<nav className={showMenu ? 'menu active' : 'menu'}>
-    <ul>
-        <NavLink to='/' className={({ isActive }) => isActive ? 'nav-link-underline' : 'nav-link'}>Inicio</NavLink>
-        <NavLink to='/categoria/remeras' className={({ isActive }) => isActive ? 'nav-link-underline' : 'nav-link'}>Remeras</NavLink>
-        <NavLink to='/categoria/zapatillas' className={({ isActive }) => isActive ? 'nav-link-underline' : 'nav-link'}>Zapatillas</NavLink>
-        <NavLink to='/categoria/camperas' className={({ isActive }) => isActive ? 'nav-link-underline' : 'nav-link'}>Camperas</NavLink>
-        <NavLink to='/categoria/conjuntos' className={({ isActive }) => isActive ? 'nav-link-underline' : 'nav-link'}>Conjuntos</NavLink>
-        <NavLink to='/cart' className='cart'>{cantidadTotal() > 0 && cantidadTotal()}🛒</NavLink>
-    </ul>
-</nav>
-</div> */}
